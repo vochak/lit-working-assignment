068d006a41e1af066ad4c27856c79cc293d4fa28
Victor Mosomi Ochako <145469157+vochak@users.noreply.github.com>
2024-03-11 15:23:24 +0300

    Add files via upload

---
+++ list assignment.py
@@ -0,0 +1,9 @@
+my_list = []
+my_list = my_list.append("10, 20, 30, 40.")
+my_list = my_list.insert(1, 15)
+list_ex = [50, 60, 70]
+my_list = my_list.extend(list_ex)
+my_list.pop
+my_list.sort()
+my_list.index(30)
+print(f"index of 30 is  {my_list}")
